b0e2853745cb39d10bcdd1ea7f33ebf5fcbdec07
jmgaviriam <jmgaviriam@gmail.com>
2023-04-05 19:38:12 -0500

    Creation of files in Users.UseCase

---
+++ OnlineShoppingPlatform/Users.UseCase/Gateway/IUserUseCase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users.UseCase.Gateway
+{
+    public interface IUserUseCase
+    {
+    }
+}
---
+++ OnlineShoppingPlatform/Users.UseCase/Gateway/Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users.UseCase.Gateway.Repository
+{
+    public interface IUserRepository
+    {
+    }
+}
