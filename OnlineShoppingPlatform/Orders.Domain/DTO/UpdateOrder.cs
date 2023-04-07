334c1eff726c500edbfae016ba0c7fca8ce70baa
jmgaviriam <jmgaviriam@gmail.com>
2023-04-07 14:54:37 -0500

    Implementation of methods GetOrderById, CreateOrder and UpdateOrder for the entity Order

--- OnlineShoppingPlatform/Orders.Domain/DTO/UpdateOrder.cs
+++ OnlineShoppingPlatform/Orders.Domain/DTO/UpdateOrder.cs
@@ -8,5 +8,22 @@ namespace Orders.Domain.DTO
 {
     public class UpdateOrder
     {
+        public string OrderId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string PaymentId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public DateTime ShippingDate { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+
+        public string ShippingAddress { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public string Status { get; set; }
     }
 }
